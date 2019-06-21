5c3e01cb2f1abff6f7b6ceb53278df5e52c94bab
Shawn Huggins <hugginsjrshawn@gmail.com>
2019-06-20 20:15:03 -0500

    Add thumbnail fetching for each manga

--- manga_list.js
+++ manga_list.js
@@ -1,11 +1,52 @@
-const COLUMN = { NAME: 0 }
+const COLUMN = {
+  NAME: 0
+}
 
-// TODO: Find a hook to start scanning though manga instead of waiting a second.
-function main() {
-  const mangaList = document.getElementsByClassName("jtable-data-row")
-  for (let i = 0; i < mangaList.length; i++) {
-    const mangaTitle = mangaList[i].childNodes[COLUMN.NAME].innerText
-    console.log(mangaTitle)
+const fetchHTML = (url) => {
+  return new Promise((resolve, reject) => {
+    window.fetch(url).then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error('Non 200 range status code recieved.'))
+      }
+      return response.text()
+    }).then(htmlString => {
+      const doc = new window.DOMParser().parseFromString(htmlString, 'text/html')
+      resolve(doc)
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+
+const fetchMangaThumbnailSrc = (mangaUrl) => {
+  return new Promise((resolve, reject) => {
+    fetchHTML(mangaUrl).then(doc => {
+      try {
+        const mangaThumbnailSrc = doc.getElementsByClassName('thumbnail')[0].src
+        resolve(mangaThumbnailSrc)
+      } catch (error) {
+        return Promise.reject(error)
+      }
+    }).catch(error => {
+      return reject(error)
+    })
+  })
+}
+
+// TODO: Find a hook to start scanning though the manga table instead of waiting a second.
+const main = () => {
+  const mangaTable = document.getElementsByClassName('jtable-data-row')
+  for (const rowIndex in mangaTable) {
+    const mangaRow = mangaTable[rowIndex].childNodes
+    const mangaTitle = mangaRow[COLUMN.NAME].innerText
+    const mangaUrl = mangaRow[COLUMN.NAME].firstElementChild.href
+
+    fetchMangaThumbnailSrc(mangaUrl).then(thumbnailSrc => {
+      console.log(thumbnailSrc)
+      console.log(mangaTitle)
+    }).catch(error => {
+      console.error(error)
+    })
   }
 }
 
